e0c187ad310ee60401c91157b08e8af663b63052
Beedhan <bhuwaibidhan@gmail.com>
2021-03-02 15:09:03 +0545

    added cors

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const app = express();
+const cors = require('cors')
 
 const jokeRouters = require('./JokeRoutes');
 
 const PORT = process.env.PORT || 5000
+
+app.use(cors())
 
 app.use('/joke',jokeRouters)
 
