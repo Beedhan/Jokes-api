76345b3925241bc6cac4d978009a1cca70d182aa
Beedhan <bhuwaibidhan@gmail.com>
2021-03-02 12:48:56 +0545

    added reddit json

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,13 +1,26 @@
 const axios = require("axios");
 
 exports.yomamma = (req, res, next) => {
-  axios.get("https://api.yomomma.info/").then((data) => {
-    res.status(200).json({ joke: data.data.joke, success: true });
-  });
+  const randomNo = (Math.floor(Math.random(0, 1) * 2))
+  if (randomNo === 0) {
+    axios.get("https://api.yomomma.info/").then((data) => {
+      res.status(200).json({ joke: data.data.joke, success: true });
+    });
+  } else {
+    axios.get("https://www.reddit.com/r/YoMamaJokes.json?limit=100").then(({ data }) => {
+      const randomPost = (Math.floor(Math.random(0, 1) * 100) + 1)
+      res.status(200).json({ joke: `${data.data.children[randomPost].data.title} ${data.data.children[randomPost].data.selftext}`, success: true });
+    });
+  }
 };
 
 exports.dadjoke = (req, res, next) => {
-  axios
+
+
+    //Reddit joke
+  const randomNo = (Math.floor(Math.random(0, 1) * 2))
+  if (randomNo === 0) {
+    axios
     .get("https://icanhazdadjoke.com/", {
       headers: { Accept: "application/json" },
     })
@@ -17,6 +30,14 @@ exports.dadjoke = (req, res, next) => {
     .catch((err) => {
       res.status(400).json({ msg: "Error Getting ", success: false });
     });
+  } else {
+    axios.get("https://www.reddit.com/r/dadjokes.json?limit=100").then(({ data }) => {
+      const randomPost = (Math.floor(Math.random(0, 1) * 100) + 1)
+      res.status(200).json({ joke: `${data.data.children[randomPost].data.title} ${data.data.children[randomPost].data.selftext}`, success: true });
+    }).catch((err) => {
+      res.status(400).json({ msg: "Error Getting ", success: false });
+    });
+  }
 };
 
 exports.programming = (req, res, next) => {
@@ -35,24 +56,38 @@ exports.programming = (req, res, next) => {
     .catch((err) => {
       res.status(400).json({ msg: "Error Getting ", success: false });
     });
+
+    
 };
 
 exports.dark = (req, res, next) => {
-  axios
-    .get("https://v2.jokeapi.dev/joke/dark")
-    .then(({ data }) => {
-      console.log(data);
-      if (data.type === "twopart") {
-        res
-          .status(200)
-          .json({ joke: `${data.setup} ,${data.delivery}`, success: true });
-      } else {
-        res.status(200).json({ joke: data.joke, success: true });
-      }
-    })
-    .catch((err) => {
+
+  //Reddit joke
+  const randomNo = (Math.floor(Math.random(0, 1) * 2))
+  if (randomNo === 0) {
+    axios
+      .get("https://v2.jokeapi.dev/joke/dark")
+      .then(({ data }) => {
+        console.log(data);
+        if (data.type === "twopart") {
+          res
+            .status(200)
+            .json({ joke: `${data.setup} ${data.delivery}`, success: true });
+        } else {
+          res.status(200).json({ joke: data.joke, success: true });
+        }
+      })
+      .catch((err) => {
+        res.status(400).json({ msg: "Error Getting ", success: false });
+      });
+  } else {
+    axios.get("https://www.reddit.com/r/darkJokes.json?limit=100").then(({ data }) => {
+      const randomPost = (Math.floor(Math.random(0, 1) * 100) + 1)
+      res.status(200).json({ joke: `${data.data.children[randomPost].data.title} ${data.data.children[randomPost].data.selftext}`, success: true });
+    }).catch((err) => {
       res.status(400).json({ msg: "Error Getting ", success: false });
     });
+  }
 };
 
 exports.doland = (req, res, next) => {
